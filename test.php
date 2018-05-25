5edb968b1488b32cca5f86a63c25e8f5c42bf3a4
Jeroen van Steijn <32145092+JeroenVanSteijn@users.noreply.github.com>
2018-05-25 18:51:43 +0200

    Create test.php

---
+++ test.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+final class Test extends TestCase
+{
+    public function testExample(): void
+    {
+        'test'->assertEquals(
+            'test'
+        );
+    }
+}
