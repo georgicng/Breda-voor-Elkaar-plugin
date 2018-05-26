5ec43968a924c803da4e5e1d3a66d2fef211ed0e
Jeroen van Steijn <32145092+JeroenVanSteijn@users.noreply.github.com>
2018-05-26 17:18:46 +0200

    Update ExampleBredaTest.php

--- ExampleBredaTest.php
+++ ExampleBredaTest.php
@@ -5,7 +5,7 @@ use PHPUnit\Framework\TestCase;
 
 class Test extends TestCase
 {
-    public function dummyTest()
+    public function testDummy()
     {
         $this->assertEquals('2204', 2204);
     }
