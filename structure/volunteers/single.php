95e91a2a73d35cc5b1116531310361e8845f8046
Jeroen van Steijn <J.J.vanSteijn@student.tudelft.nl>
2018-05-30 16:33:33 +0200

    Add single template for usertypes

--- structure/volunteers/single.php
+++ structure/volunteers/single.php
@@ -1,12 +1,43 @@
-<?php
-//Header?
+<?php //get_header(); ?>
 
+<?php $ID = get_queried_object()->ID; ?>
+<?php $usermeta = get_user_meta($ID); ?>
+
+<!-- Body content -->
+
+<?php
+    global $post; 
+    $post = get_post( $ID, OBJECT );
+    setup_postdata( $post );
+?>
+    <li>
+        ID: <?php echo $ID; ?>
+    </li>
+    <li>
+        posted on: <?php the_time('d M Y'); ?>
+    </li>
+    <li>
+        login name: <?php echo $usermeta['nickname'][0]; ?>
+    </li>
+    <li>
+        first name: <?php echo $usermeta['first_name'][0]; ?>
+    </li>
+    <li>
+        last name: <?php echo $usermeta['last_name'][0]; ?>
+    </li>
+    <li>
+        age: <?php the_field('leeftijd'); ?>
+    </li>
+    <li>
+        addres: 
+    </li>
+    <li>
+        opleiding: <?php echo the_field('opleiding'); ?>
+    </li>
+<?
+wp_reset_postdata();
 ?>
 
-<?php
-//Body
+<!-- End body content -->
 
-?>
-
-<?php
-//Footer?
+<?php //get_footer(); ?>
