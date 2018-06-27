e5dff16e0a7457951838e1e7a5cbade3bb0e5599
Jeroen van Steijn <J.J.vanSteijn@student.tudelft.nl>
2018-06-27 11:42:09 +0200

    Add menu to my-acccount

--- structure/my-account/template.blade.php
+++ structure/my-account/template.blade.php
@@ -7,10 +7,27 @@
 
 <?php
 if(is_user_logged_in()){
+    $user = wp_get_current_user();
+    $role = ( array ) $user->roles;
+    if($role[0] == 'organisation'){
     ?>
-    <div id="my-account">
-    </div>
+    <ul id="account-menu">
+        <li><a href="/beheer-vacatures">Beheer vacatures</a></li>
+        <li><a href="/nieuwe-vacature">Nieuwe vacature plaatsen</a></li>
+        <li><a href="/wijzig-wachtwoord">Wijzig wachtwoord</a></li>
+        <li><a href="<?php echo wp_logout_url( home_url() ); ?>">Logout</a></li>
+    </ul>
     <?php
+    } else if($role[0] == 'volunteer'){
+    ?>
+    <ul id="account-menu">
+        <li><a href="/wijzig-wachtwoord">Wijzig wachtwoord</a></li>
+        <li><a href="<?php echo wp_logout_url( home_url() ); ?>">Logout</a></li>
+    </ul>
+    <?php
+    } else{
+        echo 'Uw account is geen vrijwilliger of organisatie.';
+    }
 } else{
     echo 'Je moet ingelogd zijn om deze pagina te bekijken.';
 }
