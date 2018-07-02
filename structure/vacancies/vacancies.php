e1d0622c4134a476f29900149ed505bee3b61655
Jeroen van Steijn <J.J.vanSteijn@student.tudelft.nl>
2018-07-02 19:13:06 +0200

    Fix template after slug change

--- structure/vacancies/vacancies.php
+++ structure/vacancies/vacancies.php
@@ -204,9 +204,10 @@ add_action('acf/init', 'register_custom_fields_vacancy');
  * Set template for archive page.
  */    
  function archive_vacancy_template( $page_template ) {
-     if ( is_page( 'Vacatures' ) ) {
-         $page_template = plugin_dir_path( __FILE__ ) . '/archive-vacancies.blade.php';
-     }
-     return $page_template;
+    if ( is_post_type_archive( 'vacancies' ) ) {
+        $page_template = plugin_dir_path( __FILE__ ) . '/archive-vacancies.blade.php';
+    }
+    return $page_template;
  }
- add_filter( 'page_template', 'archive_vacancy_template' );
+ add_filter( 'archive_template', 'archive_vacancy_template' );
+ 
