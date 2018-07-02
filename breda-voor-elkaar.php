95780d2ceed33c3f7f2ca374577f8634db0bc5de
Jeroen van Steijn <J.J.vanSteijn@student.tudelft.nl>
2018-07-02 20:15:48 +0200

    Remove admin bar

--- breda-voor-elkaar.php
+++ breda-voor-elkaar.php
@@ -194,3 +194,14 @@ function filter_script($page){
 
     <?php
 }
+
+/**
+ * Disable the administrator bar for non-admins.
+ */
+function remove_admin_bar() {
+    if (!current_user_can('administrator') && !is_admin()) {
+      show_admin_bar(false);
+    }
+}
+add_action('after_setup_theme', 'remove_admin_bar');
+ 
