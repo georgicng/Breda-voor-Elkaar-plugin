6b3fabc1b86205313a40b8c433679da7a0c9dc80
Jeroen van Steijn <J.J.vanSteijn@student.tudelft.nl>
2018-07-03 21:26:22 +0200

    Fix first value for application and favorites

--- structure/vacancies/single.blade.php
+++ structure/vacancies/single.blade.php
@@ -75,16 +75,18 @@ if(isset($_POST['Favoriet'])){
     $user_id = $_POST['user_id'];
     $post_id = $_POST['post_id'];
     $current_array = get_field('field_5b0frsad5dfc7','user_'.$user_id);
-    array_push($current_array,$post_id);
+    $current_array = (array)$current_array;
     if(!in_array($post_id,$current_array)){
+        $current_array[] = $post_id;
         update_field('field_5b0frsad5dfc7', $current_array, 'user_'.$user_id);
     }
 } else if(isset($_POST['Reageer'])){
     $user_id = $_POST['user_id'];
     $post_id = $_POST['post_id'];
     $current_array = get_field('field_5b0fe6fd5dfc7','user_'.$user_id);
-    array_push($current_array,$post_id);
+    $current_array = (array)$current_array;
     if(!in_array($post_id,$current_array)){
+        $current_array[] = $post_id;
         update_field('field_5b0fe6fd5dfc7', $current_array, 'user_'.$user_id);
     }
 }
