113ce0d602ca9dec4c6f128be6215f4d6b6b9402
Jeroen van Steijn <J.J.vanSteijn@student.tudelft.nl>
2018-07-03 13:20:38 +0200

    Add check for old password to password change

--- structure/my-account/change-password.blade.php
+++ structure/my-account/change-password.blade.php
@@ -17,29 +17,37 @@ if(is_user_logged_in()){
     $user_id = $user->ID;  
     $meta = get_user_meta($user_id, 'profile', true);
     $output='';
-    if(isset($_POST['changepass'])){
-        if($_POST['npass']!=$_POST['cpass']){
-            $output='Uw wachtwoorden kwamen niet overeen';    
+    if(isset($_POST['changepass'] && isset($_POST['opass'])){
+        if(wp_check_password($_POST['opass'], $user->data->user_pass,  $user->ID)){
+            if($_POST['npass']!=$_POST['cpass']){
+                $output='Uw wachtwoorden kwamen niet overeen';    
+            } else{
+              if($_POST['npass']==''){
+                $password=$_POST['password'];
+              }  
+              else{
+                $password=$_POST['npass'];
+              }
+              wp_set_password( $password, $user_id );
+              $output='Uw wachtwoord is veranderd';
+            } 
         } else{
-          if($_POST['npass']==''){
-            $password=$_POST['password'];
-          }  
-          else{
-            $password=$_POST['npass'];
-          }
-          wp_set_password( $password, $user_id );
-          $output='Uw wachtwoord is veranderd';
-        } 
+            $output = 'Uw oude wachtwoord was incorrect';
+        }
     }
     ?>
     <form class="password_change"  method="post" enctype="multipart/form-data" action="#" onsubmit="return change_password();">
-        <h2>Change Password</h2>
+        <h2>Wachtwoord Aanpassen</h2>
         <div class="form-field">
-            <label>New Password</label>
+            <label>Oude Wachtwoord</label>
+            <input type="password" name="opass" id="opass" />
+        </div>
+        <div class="form-field">
+            <label>Nieuw Wachtwoord</label>
             <input type="password" name="npass" id="pass" />
         </div>
         <div class="form-field">
-            <label>Confirm Password</label>
+            <label>Bevestig Wachtwoord</label>
             <input type="password" name="cpass" id="cpass" />
         </div>
         <div id="error"><?php echo $output; ?></div>
