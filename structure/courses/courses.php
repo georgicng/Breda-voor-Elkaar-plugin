c5260c831bf288eb94873fee1afc940d6a097557
George <george@gaiproject.com>
2018-07-26 23:14:08 +0100

    Added courses meta for homepage and disabled vacancy template override to default to theme template

--- structure/courses/courses.php
+++ structure/courses/courses.php
@@ -45,84 +45,171 @@ add_action('init', 'create_post_type_course');
 function register_custom_fields_course()
 {
     if (function_exists('acf_add_local_field_group')) {
-        acf_add_local_field_group(array(
-            'key' => 'group_5b5722ba58a81',
-            'title' => 'Course',
-            'fields' => array(
-                array(
-                    'key' => 'field_5b5722cac4d7c',
-                    'label' => 'Date',
-                    'name' => 'date',
-                    'type' => 'date_picker',
-                    'instructions' => '',
-                    'required' => 1,
-                    'conditional_logic' => 0,
-                    'wrapper' => array(
-                        'width' => '',
-                        'class' => '',
-                        'id' => '',
-                    ),
-                    'display_format' => 'd/m/Y',
-                    'return_format' => 'd/m/Y',
-                    'first_day' => 1,
-                ),
-                array(
-                    'key' => 'field_5b572314c4d7d',
-                    'label' => 'Lesson',
-                    'name' => 'lesson',
-                    'type' => 'text',
-                    'instructions' => '',
-                    'required' => 0,
-                    'conditional_logic' => 0,
-                    'wrapper' => array(
-                        'width' => '',
-                        'class' => '',
-                        'id' => '',
-                    ),
-                    'default_value' => '',
-                    'placeholder' => '',
-                    'prepend' => '',
-                    'append' => '',
-                    'maxlength' => '',
-                ),
-                array(
-                    'key' => 'field_5b572364c4d7e',
-                    'label' => '',
-                    'name' => '',
-                    'type' => 'text',
-                    'instructions' => '',
-                    'required' => 0,
-                    'conditional_logic' => 0,
-                    'wrapper' => array(
-                        'width' => '',
-                        'class' => '',
-                        'id' => '',
-                    ),
-                    'default_value' => '',
-                    'placeholder' => '',
-                    'prepend' => '',
-                    'append' => '',
-                    'maxlength' => '',
-                ),
-            ),
-            'location' => array(
-                array(
+        acf_add_local_field_group(
+            array(
+                'key' => 'group_5b5722ba58a81',
+                'title' => 'Course',
+                'fields' => array(
                     array(
-                        'param' => 'post_type',
-                        'operator' => '==',
-                        'value' => 'course',
+                        'key' => 'field_5b5722cac4d7c',
+                        'label' => 'Date',
+                        'name' => 'date',
+                        'type' => 'date_picker',
+                        'instructions' => '',
+                        'required' => 1,
+                        'conditional_logic' => 0,
+                        'wrapper' => array(
+                            'width' => '',
+                            'class' => '',
+                            'id' => '',
+                        ),
+                        'display_format' => 'd/m/Y',
+                        'return_format' => 'd/m/Y',
+                        'first_day' => 1,
+                    ),
+                    array(
+                        'key' => 'field_5b572314c4d7d',
+                        'label' => 'Lesson',
+                        'name' => 'lesson',
+                        'type' => 'text',
+                        'instructions' => '',
+                        'required' => 0,
+                        'conditional_logic' => 0,
+                        'wrapper' => array(
+                            'width' => '',
+                            'class' => '',
+                            'id' => '',
+                        ),
+                        'default_value' => '',
+                        'placeholder' => '',
+                        'prepend' => '',
+                        'append' => '',
+                        'maxlength' => '',
+                    ),
+                    array(
+                        'key' => 'field_5b572364c4d7e',
+                        'label' => '',
+                        'name' => '',
+                        'type' => 'text',
+                        'instructions' => '',
+                        'required' => 0,
+                        'conditional_logic' => 0,
+                        'wrapper' => array(
+                            'width' => '',
+                            'class' => '',
+                            'id' => '',
+                        ),
+                        'default_value' => '',
+                        'placeholder' => '',
+                        'prepend' => '',
+                        'append' => '',
+                        'maxlength' => '',
                     ),
                 ),
-            ),
-            'menu_order' => 0,
-            'position' => 'normal',
-            'style' => 'default',
-            'label_placement' => 'top',
-            'instruction_placement' => 'label',
-            'hide_on_screen' => '',
-            'active' => 1,
-            'description' => '',
-        ));
+                'location' => array(
+                    array(
+                        array(
+                            'param' => 'post_type',
+                            'operator' => '==',
+                            'value' => 'course',
+                        ),
+                    ),
+                ),
+                'menu_order' => 0,
+                'position' => 'normal',
+                'style' => 'default',
+                'label_placement' => 'top',
+                'instruction_placement' => 'label',
+                'hide_on_screen' => '',
+                'active' => 1,
+                'description' => '',
+            )
+        );
+
+        acf_add_local_field_group(
+            array(
+                'key' => 'group_5b5a18ac28cdd',
+                'title' => 'Front Page',
+                'fields' => array(
+                    array(
+                        'key' => 'field_5b5a18eeaf710',
+                        'label' => 'Course Title',
+                        'name' => 'course_title',
+                        'type' => 'text',
+                        'instructions' => '',
+                        'required' => 0,
+                        'conditional_logic' => 0,
+                        'wrapper' => array(
+                            'width' => '',
+                            'class' => '',
+                            'id' => '',
+                        ),
+                        'default_value' => 'Cursussen',
+                        'placeholder' => '',
+                        'prepend' => '',
+                        'append' => '',
+                        'maxlength' => '',
+                    ),
+                    array(
+                        'key' => 'field_5b5a1929af711',
+                        'label' => 'Sidebar Title',
+                        'name' => 'course_subtitle',
+                        'type' => 'text',
+                        'instructions' => '',
+                        'required' => 0,
+                        'conditional_logic' => 0,
+                        'wrapper' => array(
+                            'width' => '',
+                            'class' => '',
+                            'id' => '',
+                        ),
+                        'default_value' => 'Intro over cursussen lorep ipsum Breda cras sectum',
+                        'placeholder' => '',
+                        'prepend' => '',
+                        'append' => '',
+                        'maxlength' => '',
+                    ),
+                    array(
+                        'key' => 'field_5b5a1955af712',
+                        'label' => 'Description',
+                        'name' => 'course_description',
+                        'type' => 'wysiwyg',
+                        'instructions' => '',
+                        'required' => 0,
+                        'conditional_logic' => 0,
+                        'wrapper' => array(
+                            'width' => '',
+                            'class' => '',
+                            'id' => '',
+                        ),
+                        'default_value' => '<p>In tempus pulvinar mattis. Sed sed feugiat metus. Nam sollicitudin risus sapien, id bibendum urna laoreet nec. Etiam efficitur libero eget nisl euismod, a rutrum leo sagittis. Mauris egestas diam purus. In tempus pulvinar mattis. Sed sed feugiat metus. Nam sollicitudin risus sapien, id bibendum urna laoreet nec. Etiam efficitur libero eget nisl euismod, a rutrum leo sagittis. Mauris egestas diam purus.</p>
+
+        <p>In tempus pulvinar mattis. Sed sed feugiat metus. Nam sollicitudin risus sapien, id bibendum urna laoreet nec. Etiam efficitur libero eget nisl euismod. </p>',
+                        'tabs' => 'all',
+                        'toolbar' => 'full',
+                        'media_upload' => 1,
+                        'delay' => 0,
+                    ),
+                ),
+                'location' => array(
+                    array(
+                        array(
+                            'param' => 'page_type',
+                            'operator' => '==',
+                            'value' => 'front_page',
+                        ),
+                    ),
+                ),
+                'menu_order' => 0,
+                'position' => 'normal',
+                'style' => 'default',
+                'label_placement' => 'top',
+                'instruction_placement' => 'label',
+                'hide_on_screen' => '',
+                'active' => 1,
+                'description' => '',
+            )
+        );
 
     }
 }
